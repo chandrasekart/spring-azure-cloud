a93d5e32e75e885e33c5d0636e4475b048941ee8
Thatchinamoorthy <chandrasekar.thatchinamoorthy@igt.com>
2023-06-20 15:48:17 +0100

    first commit

---
+++ src/main/java/com/example/springazurecloud/SpringAzureCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springazurecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureCloudApplication.class, args);
+	}
+
+}
